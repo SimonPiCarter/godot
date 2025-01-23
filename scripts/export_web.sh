e8edcc8d5eb65178735d3599d97c3759590dcef5
simon <simon@DESKTOP-AL0PF2L.lan>
2025-01-24 00:23:52 +0100

    improved web export

--- scripts/export_web.sh
+++ scripts/export_web.sh
@@ -1,1 +1,2 @@
 ~/dev/godot/bin/godot.linuxbsd.editor.x86_64 --path /home/simon/godot_workspace/godot_octopus2/ --export-release "Web" ./bin/export/html/index.html --headless
+zip /home/simon/godot_workspace/godot_octopus2/bin/export/html.zip /home/simon/godot_workspace/godot_octopus2/bin/export/html/*
