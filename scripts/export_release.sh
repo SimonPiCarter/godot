b1fb43b33d91b7c7a59cd19afcf749f0e9c2ca30
simon <simon@DESKTOP-AL0PF2L.lan>
2025-06-13 23:16:54 +0200

    remove web export

--- scripts/export_release.sh
+++ scripts/export_release.sh
@@ -4,4 +4,4 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 sh ${SCRIPT_DIR}/export_release_linux.sh
 sh ${SCRIPT_DIR}/export_release_windows.sh
-sh ${SCRIPT_DIR}/export_web.sh
+#sh ${SCRIPT_DIR}/export_web.sh
